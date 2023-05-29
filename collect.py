d24a1abb579f9eb6010e5780043f30ba07c009d7
ging-dev <thanh1101dev@gmail.com>
2023-05-29 23:12:00 +0700

    first commit

---
+++ collect.py
@@ -0,0 +1,40 @@
+import argparse
+import cv2
+import csv
+from lib import camera, BreakException
+from typing import Tuple
+
+def generate_training_set(image: cv2.Mat, hand_detect: Tuple[bool, list[float]]):
+    global isCapture, i, writer, class_name
+
+    has_hand, distances = hand_detect
+    cv2.putText(image, f'{i}/{args.iter}', (50, 100),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+    if isCapture and has_hand and i < args.iter:
+        distances.append(class_name)
+        writer.writerow(distances)
+        i += 1
+
+    cv2.imshow("collect", image)
+
+    match cv2.waitKey(5) & 0xFF:
+        case 115:
+            isCapture = True
+        case 27:
+            raise BreakException("Shutdown...")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Training data generator')
+    parser.add_argument('name', type=int)
+    parser.add_argument('-i', '--iter', type=int, default=100)
+
+    args = parser.parse_args()
+    i = 0
+    isCapture = False
+    class_name = args.name
+
+    with open('train.csv', 'a') as file:
+        writer = csv.writer(file)
+        camera(generate_training_set)
---
+++ lib/utils.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+from math import sqrt
+from mediapipe.python.solutions import hands
+from typing import Tuple, Callable
+
+
+class BreakException(Exception):
+    pass
+
+
+class Camera():
+    def __init__(self) -> None:
+        self.vid = cv2.VideoCapture(0)
+
+    def __enter__(self):
+        print('Camera is open.')
+        return self.vid
+
+    def __exit__(self, *args):
+        self.vid.release()
+        cv2.destroyAllWindows()
+        print('Camera is closed.')
+
+
+def camera(fn: Callable[[cv2.Mat, Tuple[bool, list[float]]], None]):
+    with Camera() as cap, hands.Hands(max_num_hands=1) as hand:
+        try:
+            while cap.isOpened():
+                success: bool
+                image: cv2.Mat
+
+                success, image = cap.read()
+
+                if not success:
+                    raise BreakException(
+                        "Can't receive frame (stream end?). Exiting ...")
+
+                image = cv2.flip(image, 1)
+
+                rgb_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                result = hand.process(rgb_img)
+
+                has_hand = False
+                distances = []
+                if result.multi_hand_landmarks:  # type: ignore
+                    has_hand = True
+                    hand_landmarks = result.multi_hand_landmarks[0] # type: ignore
+
+                    points = [(int(landmark.x*image.shape[1]), int(landmark.y*image.shape[0]))
+                              for landmark in hand_landmarks.landmark]
+
+                    (x, y), r = cv2.minEnclosingCircle(np.array(points))
+
+                    wist = hand_landmarks.landmark[0]
+                    distances = [sqrt((wist.x-other.x)**2+(wist.y-other.y)**2+(wist.z-other.z)**2)
+                                 for other in hand_landmarks.landmark[1:]]
+
+                    x_min, y_min = int(x - r), int(y - r)
+                    x_max, y_max = int(x + r), int(y + r)
+
+                    image = cv2.rectangle(
+                        image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+
+                fn(image, (has_hand, distances))
+
+        except BreakException as e:
+            print(e)
